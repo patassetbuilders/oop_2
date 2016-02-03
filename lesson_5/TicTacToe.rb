ac37f34ba4d7e2f750e3a6f0ab0a1f3be9ece9e2
patassetbuilders <pjames@assetbuilders.com.au>
2016-02-04 07:17:31 +1000

    new feature - implementing 5 game match

--- lesson_5/TicTacToe.rb
+++ lesson_5/TicTacToe.rb
@@ -118,6 +118,7 @@ class TTTGame
   HUMAN_MARKER = 'X'
   COMPUTER_MARKER = "O"
   FIRST_TO_MOVE = HUMAN_MARKER
+  GAMES_PER_MATCH = 5
   attr_reader :board, :human, :computer
 
   def initialize
@@ -129,17 +130,23 @@ class TTTGame
 
   def play
     display_welcome_message
-    loop do
-      display_board
-      loop do
-        current_player_moves
-        break if board.someone_won? || board.full?
-        clear_screen_and_display_board
+    loop do # play again 
+      loop do # first to five
+        display_board
+        loop do # players take turns
+          current_player_moves
+          break if board.someone_won? || board.full?
+          clear_screen_and_display_board
+        end
+        update_score
+        
+        display_result
+        game_reset
+        break if match_over? 
       end
-      display_result
-      break unless play_again?
-      game_reset
       display_play_again_message
+      break unless play_again?
+      reset_score
     end
     display_goodbye_message
   end
@@ -147,7 +154,7 @@ class TTTGame
   private
 
   def clear
-    system 'clear'
+    system "clear"
   end
 
   def current_player_moves
@@ -193,20 +200,34 @@ class TTTGame
   def computer_moves
     board.[]=(board.unmarked_keys.sample, computer.marker)
   end
+  
+  def update_score
+    case board.winning_marker
+      when human.marker
+        human.number_of_wins += 1
+      when computer.marker
+        computer.number_of_wins += 1
+      end
+  end
+  
+  def reset_score
+    human.number_of_wins = 0
+    computer.number_of_wins = 0
+  end
 
   def display_result
     clear_screen_and_display_board
     case board.winning_marker
     when human.marker
       puts "You won!"
-      human.number_of_wins += 1
     when computer.marker
       puts "Computer won"
-      computer.number_of_wins += 1
     else
       puts "Its a tie"
     end
-    puts "Current score You #{human.number_of_wins} V Computer #{computer.number_of_wins}" 
+    puts "Current Score  - YOU #{human.number_of_wins}  Computer #{computer.number_of_wins}" 
+    puts "Winner is first to 5"
+    sleep 3
   end
 
   def game_reset
@@ -230,8 +251,18 @@ class TTTGame
     end
     answer == 'y'
   end
+  
+  def match_over?
+    human.number_of_wins == GAMES_PER_MATCH || computer.number_of_wins == GAMES_PER_MATCH
+  end
 
   def display_goodbye_message
+    case human.number_of_wins 
+      when GAMES_PER_MATCH
+        puts "You win #{human.number_of_wins} games to #{computer.number_of_wins}"
+      else
+        puts "Computer wins #{computer.number_of_wins} games to #{human.number_of_wins}"
+      end 
     puts "Thank you for playing Tic Tac Toe - Goodbye for now"
   end
 end
